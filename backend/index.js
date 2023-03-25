fbfeb23bb005cb5624da510da0e0b939116fbd57
Rajan Chaudhary <60035342+rajann44@users.noreply.github.com>
2023-03-25 22:14:47 +0100

    mongoosejs routes and models

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,9 @@ const port = 3000
 
 connectToMongo();
 
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })

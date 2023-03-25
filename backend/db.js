714457ae2aa58928412577f630b018640e9ea918
Rajan Chaudhary <60035342+rajann44@users.noreply.github.com>
2023-03-25 21:04:56 +0100

    mongoose and mongodb support: npm i -D nodemon

---
+++ backend/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const mongouri = "mongodb://127.0.0.1/react-local-db"
+
+const connectToMongo = async () => {
+    mongoose.connect(mongouri).then(()=>{
+        console.log("MongoDB is connected successfully!!")
+    })
+
+}
+
+module.exports = connectToMongo;

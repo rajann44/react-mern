7687bfb59b6607a63905691b33a49c4e8e164aad
Rajan Chaudhary <60035342+rajann44@users.noreply.github.com>
2023-03-26 14:10:58 +0200

    /login api and getting authToken

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -50,7 +50,50 @@ router.post(
 			res.json({ authToken, email: user.email });
 		} catch (error) {
 			console.error(error.message);
-			res.status(500).send("Some Error Occured :(");
+			res.status(500).send("Internal Server Error Occured :(");
+		}
+	}
+);
+
+//Authenticate a user using: POST "/api/auth/login" (Require no auth / No Login required)
+router.post(
+	"/login",
+	[
+		body("email", "Enter Valid Email...").isEmail(),
+		body("password", "Password cannot be blank...").exists(),
+	],
+	async (req, res) => {
+		// If Errors return Bad request and error
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(403).json({ errors: errors.array() });
+		}
+
+		const { email, password } = req.body;
+
+		try {
+			let user = await Users.findOne({ email });
+			if (!user) {
+				return res.status(403).json({ error: "Email Or Password incorrect" });
+			}
+			const passwordComparisonResult = await bcrypt.compare(
+				password,
+				user.password
+			);
+			if (!passwordComparisonResult) {
+				return res.status(403).json({ error: "Email Or Password incorrect" });
+			}
+
+			const data = {
+				user: {
+					id: user.id,
+				},
+			};
+			const authToken = jwt.sign(data, JWT_SECRET);
+			res.json({ authToken, email: user.email });
+		} catch (error) {
+			console.error(error.message);
+			res.status(500).send("Internal Server Error Occured :(");
 		}
 	}
 );
